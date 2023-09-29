cc0106806f244d9a9fe5a949c770719a981cb644
travolgi <travolgi@gmail.com>
2023-09-29 17:32:09 +0200

    commit code optimization and fix design

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { ref, onValue, remove, update } from 'firebase/database';
 import { db, auth } from '../firebase';
 import useIsOpen from '../hooks/useIsOpen'
+import FormUI from './FormUI';
 
 export default function ExpenseList({ type, yearActive, monthActive, btn=true }) {
 	const initVals = { id: '', name: '', price: '', date: '' };
@@ -76,7 +77,7 @@ export default function ExpenseList({ type, yearActive, monthActive, btn=true })
 				queryMonth = monthActive === 'all' ? expenseMonth : monthActive,
 				expensePath = `/users/${auth.currentUser.uid}/${type}/${yearActive}/${queryMonth}/${expenseId}`
 
-		if(window.confirm('You want to delete this expense?')) {
+		if(window.confirm(`You want to delete this expense?`)) {
 			remove(ref(db, expensePath))
 				.then(() => console.log('Successfully deleted!'))
 				.catch(err => console.log(err));
@@ -90,46 +91,13 @@ export default function ExpenseList({ type, yearActive, monthActive, btn=true })
 					{data.map(expense => 
 						<li key={expense.id} className={editing && expense.id === editData.id ? 'visible' : ''}>
 							{editing && expense.id === editData.id ?
-								<>
-									<form onSubmit={handleUpdate}>
-										<input
-											type="text"
-											minLength="3"
-											placeholder={type !== 'earnings' ? 'Expense name' : 'Earning name'}
-											id="name"
-											value={editData.name}
-											onChange={handleChangeEdit}
-											required
-										/>
-
-										<input
-											type="number"
-											step="any"
-											placeholder="€ price"
-											id="price"
-											value={editData.price}
-											onChange={handleChangeEdit}
-											required
-										/>
-
-
-										<input
-											type="date"
-											id="date"
-											value={editData.date}
-											onChange={handleChangeEdit}
-											required
-										/>
-
-										<div>
-											<button type="submit">
-												<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M511.6 36.86l-64 415.1c-1.5 9.734-7.375 18.22-15.97 23.05c-4.844 2.719-10.27 4.097-15.68 4.097c-4.188 0-8.319-.8154-12.29-2.472l-122.6-51.1l-50.86 76.29C226.3 508.5 219.8 512 212.8 512C201.3 512 192 502.7 192 491.2v-96.18c0-7.115 2.372-14.03 6.742-19.64L416 96l-293.7 264.3L19.69 317.5C8.438 312.8 .8125 302.2 .0625 289.1s5.469-23.72 16.06-29.77l448-255.1c10.69-6.109 23.88-5.547 34 1.406S513.5 24.72 511.6 36.86z"></path></svg>
-											</button>
-
-											<button className="delete" onClick={handleCloseEditing}> Cancel</button>
-										</div>
-									</form>
-								</>
+								<FormUI
+									handleSubmit={handleUpdate}
+									type={type}
+									valueData={editData}
+									handleChange={handleChangeEdit}
+									handleReset={handleCloseEditing}
+								/>
 								:
 								<>
 									<span>{expense.name}</span>
@@ -137,19 +105,23 @@ export default function ExpenseList({ type, yearActive, monthActive, btn=true })
 									<time>{expense.date}</time>
 
 									{btn &&
-										<div>
+										<div className="commands">
 											<button
 												className="delete"
 												id={expense.id}
 												value={expense.date.slice(5,7)}
 												onClick={handleDelete}
-											/>
+											>
+												<span id={expense.id}>Delete</span>
+											</button>
 											
 											<button
 												className="edit"
 												id={expense.id}
 												onClick={() => handleOpenEditing(expense)}
-											/>
+											>
+												<span id={expense.id}>Edit</span>
+											</button>
 										</div>
 									}
 								</>
@@ -164,7 +136,7 @@ export default function ExpenseList({ type, yearActive, monthActive, btn=true })
 				</>
 			:
 				<li className="no-expense">
-					<span>No Expenses</span>
+					<span>No {type !== 'earnings' && type !== 'job' ? 'Expenses' : 'Earnings'}</span>
 					<strong>€ -</strong>
 					<time>gg/mm/aaaa</time>
 				</li>
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { ref, set } from 'firebase/database';
 import { db, auth } from '../firebase';
 import { uid } from 'uid';
+import FormUI from './FormUI';
 
 export default function Form({ type, handleChangeDate }) {
 	const initVals = { name: '', price: '', date: new Date().toISOString().split('T')[0] }
@@ -11,6 +12,7 @@ export default function Form({ type, handleChangeDate }) {
 		const { id, value } = e.target;
 		setNewExpense({ ...newExpense, [id]: value });
 	}
+	const handleReset = () => setNewExpense(initVals);
 
 	const addUserExpense = e => {
 		e.preventDefault();
@@ -42,38 +44,12 @@ export default function Form({ type, handleChangeDate }) {
 	};
 
 	return (
-		<form onSubmit={addUserExpense}>
-			<input
-				type="text"
-				minLength="3"
-				placeholder={type !== 'earnings' ? 'Expense name' : 'Earning name'}
-				id="name"
-				value={newExpense.name}
-				onChange={handleChange}
-				required
-			/>
-
-			<input
-				type="number"
-				step="any"
-				placeholder="€ price"
-				id="price"
-				value={newExpense.price}
-				onChange={handleChange}
-				required
-			/>
-			
-			<input
-				type="date"
-				id="date"
-				value={newExpense.date}
-				onChange={handleChange}
-				required
-			/>
-
-			<button type="submit">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M511.6 36.86l-64 415.1c-1.5 9.734-7.375 18.22-15.97 23.05c-4.844 2.719-10.27 4.097-15.68 4.097c-4.188 0-8.319-.8154-12.29-2.472l-122.6-51.1l-50.86 76.29C226.3 508.5 219.8 512 212.8 512C201.3 512 192 502.7 192 491.2v-96.18c0-7.115 2.372-14.03 6.742-19.64L416 96l-293.7 264.3L19.69 317.5C8.438 312.8 .8125 302.2 .0625 289.1s5.469-23.72 16.06-29.77l448-255.1c10.69-6.109 23.88-5.547 34 1.406S513.5 24.72 511.6 36.86z"></path></svg>
-			</button>
-		</form>
+		<FormUI
+			handleSubmit={addUserExpense}
+			type={type}
+			valueData={newExpense}
+			handleChange={handleChange}
+			handleReset={handleReset}
+		/>
 	);
 }
